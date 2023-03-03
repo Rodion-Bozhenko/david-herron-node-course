f8cc114afb17b83515599a6697e38cf7f068da63
LemmyOsborne <bozhenko.rodion@mail.ru>
2023-03-03 22:55:19 +0400

    Initialize socket io with passport.js and add socket io to home router

--- notes/app.mjs
+++ notes/app.mjs
@@ -1,45 +1,79 @@
-import express from "express"
-import hbs from "hbs"
-import * as path from "path"
-import logger from "morgan"
-import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
-import * as http from "http"
+import express from "express";
+import hbs from "hbs";
+import * as path from "path";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import * as http from "http";
 import {
   normalizePort,
   onError,
   onListening,
   handle404,
   basicErrorHandler
-} from "./appsupport.mjs"
-import {router as indexRouter} from "./routes/index.mjs"
-import {router as notesRouter} from "./routes/notes.mjs"
-import {router as usersRouter, initPassport} from "./routes/users.mjs"
-import rfs from "rotating-file-stream"
-import DBG from "debug"
-import {useModel as useNotesModel} from "./models/notes-store.mjs"
-import session from "express-session"
-import sessionFileStore from "session-file-store"
-import {approotdir} from "./approotdir.mjs"
-import dotenv from "dotenv/config.js"
+} from "./appsupport.mjs";
+import { router as indexRouter, init as homeInit } from "./routes/index.mjs";
+import { router as notesRouter, init as notesInit } from "./routes/notes.mjs";
+import { router as usersRouter, initPassport } from "./routes/users.mjs";
+import rfs from "rotating-file-stream";
+import DBG from "debug";
+import { useModel as useNotesModel } from "./models/notes-store.mjs";
+import session from "express-session";
+import sessionFileStore from "session-file-store";
+import { approotdir } from "./approotdir.mjs";
+import dotenv from "dotenv/config.js";
+import { Server } from "socket.io";
+import passport from "passport";
 
-const __dirname = approotdir
+const __dirname = approotdir;
 
-const FileStore = sessionFileStore(session)
-export const sessionCookieName = "notescookie.sid"
+const FileStore = sessionFileStore(session);
+export const sessionCookieName = "notescookie.sid";
+
+const sessionMiddleware = session({
+  store: new FileStore({ path: "sessions" }),
+  secret: "keyboard mouse",
+  resave: true,
+  saveUninitialized: true,
+  name: sessionCookieName
+});
 
 useNotesModel(process.env.NOTES_MODEL || "memory")
   .then(() => {
+    homeInit();
+    notesInit();
   })
-  .catch((error) => onError({code: "ENOTESSTORE", error}))
+  .catch((error) => onError({ code: "ENOTESSTORE", error }));
 
-const debug = DBG("notes:debug")
-export const app = express()
+const debug = DBG("notes:debug");
+export const app = express();
+
+export const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+export const server = http.createServer(app);
+server.listen(port);
+server.on("error", (err) => onError(err, port));
+server.on("listening", () => onListening(server));
+
+server.on("request", (req) => {
+  debug(`${new Date().toISOString()} request ${req.method}
+   ${req.url}`);
+});
+app.use(sessionMiddleware);
+initPassport(app);
+export const io = new Server(server);
+const wrap = (middleware) => (socket, next) =>
+  middleware(socket.request, {}, next);
+io.use(wrap(sessionMiddleware));
+io.use(wrap(passport.initialize()));
+io.use(wrap(passport.session()));
+
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "hbs")
-hbs.registerPartials(path.join(__dirname, "partials"))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+hbs.registerPartials(path.join(__dirname, "partials"));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(
@@ -52,54 +86,34 @@ app.use(
       })
       : process.stdout
   })
-)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cookieParser())
-app.use(session({
-  store: new FileStore({path: "sessions"}),
-  secret: "keyboard mouse",
-  resave: true,
-  saveUninitialized: true,
-  name: sessionCookieName
-}))
-initPassport(app)
-app.use(express.static(path.join(__dirname, "public")))
+);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   "/assets/vendor/bootstrap",
   express.static(path.join(__dirname, "node_modules", "bootstrap", "dist"))
-)
+);
 app.use(
   "/assets/vendor/jquery",
   express.static(path.join(__dirname, "node_modules", "jquery", "dist"))
-)
+);
 app.use(
   "/assets/vendor/popper.js",
   express.static(
     path.join(__dirname, "node_modules", "popper.js", "dist", "umd")
   )
-)
+);
 app.use(
   "/assets/vendor/feather-icons",
   express.static(path.join(__dirname, "node_modules", "feather-icons", "dist"))
-)
+);
 // Router function lists
-app.use("/", indexRouter)
-app.use("/notes", notesRouter)
-app.use("/users", usersRouter)
+app.use("/", indexRouter);
+app.use("/notes", notesRouter);
+app.use("/users", usersRouter);
 // error handlers
 // catch 404 and forward to error handler
-app.use(handle404)
-app.use(basicErrorHandler)
-export const port = normalizePort(process.env.PORT || "3000")
-app.set("port", port)
-
-export const server = http.createServer(app)
-server.listen(port)
-server.on("error", (err) => onError(err, port))
-server.on("listening", () => onListening(server))
-
-server.on("request", (req) => {
-  debug(`${new Date().toISOString()} request ${req.method}
-   ${req.url}`)
-})
+app.use(handle404);
+app.use(basicErrorHandler);
--- notes/routes/notes.mjs
+++ notes/routes/notes.mjs
@@ -1,8 +1,10 @@
-import express from "express"
-import {NotesStore as notes} from "../models/notes-store.mjs"
-import {ensureAuthenticated, twitterLogin} from "./users.mjs"
+import express from "express";
+import { NotesStore as notes } from "../models/notes-store.mjs";
+import { ensureAuthenticated, twitterLogin } from "./users.mjs";
+import DBG from "debug";
 
-export const router = express.Router()
+const debug = DBG("notes:debug");
+export const router = express.Router();
 
 // Add note
 router.get("/add", ensureAuthenticated, (req, res) => {
@@ -13,42 +15,42 @@ router.get("/add", ensureAuthenticated, (req, res) => {
     user: req.user,
     twitterLogin,
     note: undefined
-  })
-})
+  });
+});
 
 router.post("/save", ensureAuthenticated, async (req, res, next) => {
   try {
-    const {docreate, notekey, title, body} = req.body
-    console.log("BODY: ", body)
+    const { docreate, notekey, title, body } = req.body;
+    debug({ docreate, notekey, title, body });
     if (docreate === "create") {
-      await notes.create(notekey, title, body)
+      await notes.create(notekey, title, body);
     } else {
-      await notes.update(notekey, title, body)
+      await notes.update(notekey, title, body);
     }
-    res.redirect("/notes/view?key=" + notekey)
+    res.redirect("/notes/view?key=" + notekey);
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
 router.get("/view", async (req, res, next) => {
   try {
-    let note = await notes.read(req.query.key)
+    let note = await notes.read(req.query.key);
     res.render("noteView", {
       title: note ? note.title : "",
       notekey: req.query.key,
       user: req.user,
       twitterLogin,
       note
-    })
+    });
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
 router.get("/edit", ensureAuthenticated, async (req, res, next) => {
   try {
-    const note = await notes.read(req.query.key)
+    const note = await notes.read(req.query.key);
     res.render("noteedit", {
       title: note ? "Edit " + note.title : "Add a Note",
       docreate: false,
@@ -56,32 +58,35 @@ router.get("/edit", ensureAuthenticated, async (req, res, next) => {
       user: req.user,
       twitterLogin,
       note
-    })
+    });
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
 router.get("/destroy", ensureAuthenticated, async (req, res, next) => {
   try {
-    const note = await notes.read(req.query.key)
+    const note = await notes.read(req.query.key);
     res.render("notedestroy", {
       title: note ? `Delete ${note.title}` : "",
       notekey: req.query.key,
       user: req.user,
       twitterLogin,
       note
-    })
+    });
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
 router.post("/destroy/confirm", ensureAuthenticated, async (req, res, next) => {
   try {
-    await notes.destroy(req.body.notekey)
-    res.redirect("/")
+    await notes.destroy(req.body.notekey);
+    res.redirect("/");
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
+
+export function init() {
+}
